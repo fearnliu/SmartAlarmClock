61680ccd9092ce18f2b705fa53adc3e4dcc15b50
Fearn Liu <16191685+fearnliu@users.noreply.github.com>
2024-12-14 21:54:21 +0800

    feat: create a new iOS App project
    macOS Sequoia 15.2 (Intel chip)
    Xcode 16.2

---
+++ SmartAlarmClockTests/SmartAlarmClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartAlarmClockTests.swift
+//  SmartAlarmClockTests
+//
+//  Created by Fearn Liu on 2024/12/14.
+//
+
+import Testing
+@testable import SmartAlarmClock
+
+struct SmartAlarmClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
